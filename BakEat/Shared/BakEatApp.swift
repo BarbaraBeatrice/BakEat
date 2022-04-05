a7084985a7ec08bf94b7e1a6efb2dff5fe25ffbe
BarbaraBeatrice <61754424+BarbaraBeatrice@users.noreply.github.com>
2022-04-05 12:06:14 +0200

    Add files via upload

---
+++ BakEat/Shared/BakEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakEatApp.swift
+//  Shared
+//
+//  Created by Barbara Beatrice on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BakEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
